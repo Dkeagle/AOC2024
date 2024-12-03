9ba458fb96388189a90f4692af5a4e2ecc69043d
Dkartes <taildeman_dylan@outlook.com>
2024-12-03 13:28:30 +0100

    End of Day02

--- aoc/day02.py
+++ aoc/day02.py
@@ -1,13 +1,29 @@
+def is_safe(levels):
+    asc = all(1 <= levels[i+1] - levels[i] <= 3 for i in range(len(levels) - 1))
+    desc = all(1 <= levels[i] - levels[i+1] <= 3 for i in range(len(levels) - 1))
+    return asc or desc
+
+def can_be_safe(levels):
+    for i in range(len(levels)):
+        edited = levels[:i] + levels[i+1:]
+        if is_safe(edited): 
+            return True
+    return False
+
 def a():
     safe = 0
     with open("./inputs/day02.txt", "r") as file:
         for line in file:
-            levels = line.split()
-            print(levels)
-            for i in range(len(levels) - 1):
-                current, next = int(levels[i]), int(levels[i+1])
-            safe += 1
+            levels = list(map(int, line.split()))
+            if is_safe(levels):
+                safe += 1
     return safe
 
 def b():
-    return
+    safe = 0
+    with open("./inputs/day02.txt", "r") as file:
+        for line in file:
+            levels = list(map(int, line.split()))
+            if is_safe(levels) or can_be_safe(levels):
+                safe += 1
+    return safe
