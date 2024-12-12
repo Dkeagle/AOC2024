77f7387315ce007d89539cdcee2df2b7553003d8
Dkartes <taildeman_dylan@outlook.com>
2024-12-12 21:33:39 +0100

    removed io_counters from monitoring as not available in WSL

--- aoc/decorators.py
+++ aoc/decorators.py
@@ -16,7 +16,6 @@ def resource_usage_monitor(func):
         initial_memory = process.memory_info().rss
         initial_vms = process.memory_info().vms
         initial_cpu_times = process.cpu_times()
-        initial_io_counters = process.io_counters()
        
         start_time = time.time()
         result = func(*args, **kwargs)
@@ -25,7 +24,6 @@ def resource_usage_monitor(func):
         final_memory = process.memory_info().rss
         final_vms = process.memory_info().vms
         final_cpu_times = process.cpu_times()
-        final_io_counters = process.io_counters()
        
         box_width = 64
 
@@ -34,7 +32,6 @@ def resource_usage_monitor(func):
         print(f"* {f'Memory usage: {(final_memory - initial_memory) / (1024 ** 2):.4f} MB':<{box_width}} *")
         print(f"* {f'Virtual memory usage: {(final_vms - initial_vms) / (1024 ** 2):.4f} MB':<{box_width}} *")
         print(f"* {f'CPU Time (User/System): {final_cpu_times.user - initial_cpu_times.user:.2f}s/{final_cpu_times.system - initial_cpu_times.system:.2f}s':<{box_width}} *")
-        print(f"* {f'Disk I/O (Read/Write): {final_io_counters.read_bytes - initial_io_counters.read_bytes} bytes/{final_io_counters.write_bytes - initial_io_counters.write_bytes} bytes':<{box_width}} *")
         print("*" * (box_width + 4) + "\n")
        
         return result
